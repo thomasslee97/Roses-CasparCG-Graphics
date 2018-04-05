8dc92a59b7f5090403963f6f543be776f35976a6
Dan Leedham <danleedham@googlemail.com>
2018-04-05 22:29:14 +0100

    UI Update

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ var tennisScore   = [{sets1: [], sets2: [],
                       pointsPlayed: 0, server: 1, tiebreak: false, gamePoint: "", firstFault: false}];
 var badminton = {match: "Badminton", subtitle: "Best of 3 Games Wins Match", player1: "Lancaster", player2: "York", game1: 0, game2:0, point1: 0, point2: 0 };
 
-var upcoming = {numberofupcoming: 5, header: "Upcoming matches", colone: "sport", coltwo: "group", colthree: "time", colfour: "points"};
+var upcoming = {numberofupcoming: 5, header: "Upcoming matches", colone: "sport", coltwo: "group", colthree: "time", colfour: "points", sortby: ""};
 
 //Clock Functions
 var stopwatch = new Stopwatch();
